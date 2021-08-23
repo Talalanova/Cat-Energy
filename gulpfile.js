1a8f72a0eccd67e02c267f1e80d75b6ec696e5ed
Talanova <pro_ego@mail.ru>
2021-08-23 16:07:44 +1000

    сборка

---
+++ gulpfile.js
@@ -0,0 +1,108 @@
+const gulp = require("gulp");
+const csso = require("postcss-csso");
+const rename = require("gulp-rename");
+const plumber = require("gulp-plumber");
+const sourcemap = require("gulp-sourcemaps");
+const less = require("gulp-less");
+const postcss = require("gulp-postcss");
+const autoprefixer = require("autoprefixer");
+const sync = require("browser-sync").create();
+const del = require("del");
+const htmlmin = require("gulp-htmlmin");
+const imagemin = require("gulp-imagemin");
+
+// Styles
+const styles = () => {
+  return gulp.src("source/less/style.less")
+    .pipe(plumber())
+    .pipe(sourcemap.init())
+    .pipe(less())
+    .pipe(postcss([
+      autoprefixer(),
+      csso()
+    ]))
+    .pipe(sourcemap.write("."))
+    .pipe(gulp.dest("build/css"))
+    .pipe(sync.stream());
+}
+exports.styles = styles;
+
+// HTML
+const html = () => {
+  return gulp.src("source/*.html")
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest("build"));
+}
+
+// Images
+const optimizeImages = () => {
+  return gulp.src("source/img/**/*.{png,jpg,svg}")
+    .pipe(imagemin([
+      imagemin.mozjpeg({progressive: true}),
+      imagemin.optipng({optimizationLevel: 3}),
+      imagemin.svgo()
+    ]))
+    .pipe(gulp.dest("build/img"))
+}
+exports.images = optimizeImages
+
+// Copy
+const copy = (done) => {
+  gulp.src([
+    "source/fonts/*.{woff2,woff}",
+    "source/*.ico",
+  ], {
+    base: "source"
+  })
+    .pipe(gulp.dest("build"))
+  done();
+}
+exports.copy = copy;
+
+// Server
+const server = (done) => {
+  sync.init({
+    server: {
+      baseDir: 'build'
+    },
+    cors: true,
+    notify: false,
+    ui: false,
+  });
+  done();
+}
+
+// Watcher
+const watcher = () => {
+  gulp.watch("source/less/**/*.less", gulp.series(styles));
+  gulp.watch("source/*.html").on("change", gulp.series(
+    html,
+    sync.reload
+  ));
+}
+
+
+
+// Clean
+const clean = () => {
+  return del("build");
+};
+
+// Build
+const build = gulp.series(
+  clean,
+  copy,
+  optimizeImages,
+  gulp.parallel(
+    styles,
+    html
+  )
+);
+exports.build = build
+
+// Default
+exports.default = gulp.series(
+  build,
+  server,
+  watcher
+);
